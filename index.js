3448fa864a034a348311dec19aa08a17d152f3fa
bblair321 <brady@BradysPC.>
2025-05-19 10:59:12 -0700

    commit to main

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// Write your code here!
+const userInfo = {
+    username: "flatuser123",
+    description: "Hi, welcome to my webpage! Just a new user of this website!",
+    theme: "red"
+}
+
+function displayUser(userinfo){
+    const header = document.querySelector("#main")
+    header.textContent = userInfo.username + " 's Personal Webpage"
+    header.className = userInfo.theme
+
+    const description = document.createElement("p")
+    const body = document.querySelector("body")
+
+    description.textContent = userInfo.description
+    description.className = userInfo.theme
+
+    body.append(description)
+}
+
+displayUser(userInfo)
 
